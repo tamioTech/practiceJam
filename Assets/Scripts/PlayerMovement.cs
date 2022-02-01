2d676c2101f1182950d2b91a3677331cffeaa99a
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-02-01 01:39:59 -0800

    crosshair

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform crosshair;
     [SerializeField] float xMaxSpeed = 10f;
     [SerializeField] float yMaxSpeed = 10f;
+    [SerializeField] float dashSpeed = 2f;
 
     float lerpTime = 1f;
+    Vector3 distFromPlayer;
+    Vector3 dfpNorm;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,29 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         InputHandler();
+        Dash();
+
+        //print(MousePosition());
+
+        distFromPlayer = MousePosition() - rb.transform.position;
+        dfpNorm = distFromPlayer.normalized;
+        print(distFromPlayer);
+
+        crosshair.position = rb.transform.position + dfpNorm;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+    private static Vector3 MousePosition()
+    {
+        Vector3 worldToScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldToScreen.z = 0f;
+        
+        return worldToScreen;
     }
 
     private void InputHandler()
@@ -30,11 +59,20 @@ public class PlayerMovement : MonoBehaviour
         float ySpeed = yMaxSpeed * yAxis;
 
         if(lerpTime < 1) { lerpTime *= 0.2f; }
-        print("lerpTime: " + lerpTime);
         xSpeed = Mathf.Lerp(rb.velocity.x, xSpeed, lerpTime);
         ySpeed = Mathf.Lerp(rb.velocity.y, ySpeed, lerpTime);
 
         rb.velocity = new Vector2(xSpeed, ySpeed); 
 
     }
+
+    private void Dash()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+        
+        print("DASHHHHH");
+        rb.AddForce(rb.velocity * dashSpeed);
+    }
+
+
 }
