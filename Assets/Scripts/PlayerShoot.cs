4c5938837b88cc708abdec43557c947138c9fdd9
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-02-02 22:11:45 -0800

    work on weapon hud

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerShoot : MonoBehaviour
 {
@@ -8,13 +10,35 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] ParticleSystem pSys;
     [SerializeField] GameObject pObj;
     [SerializeField] Transform gunPos;
+    [SerializeField] GameObject pistolHUD;
+    [SerializeField] GameObject machineGunHUD;
+    [SerializeField] GameObject fireballHUD;
 
-    int gunSelect = 0;
+    Color pc;
+    Color mg;
+    Color fb;
+    Color pc30 = new Color(1f,1f,1f, .3f);
+    Color mg30 = new Color(1f, 1f, 1f, .3f);
+    Color fb30 = new Color(1f, 1f, 1f, .3f);
+    Color pc100 = new Color(1f, 1f, 1f, 1f);
+    Color mg100 = new Color(1f, 1f, 1f, 1f);
+    Color fb100 = new Color(1f, 1f, 1f, 1f);
+
+    int gunSelect;
 
     // Start is called before the first frame update
     void Start()
     {
+        gunSelect = 0 ;
         pSys.Play();
+
+        pistolHUD.GetComponent<Image>().color =  new Color(1f, 1f, 1f, 1f);
+        machineGunHUD.GetComponent<Image>().color = new Color(1f, 1f, 1f, .3f);
+        fireballHUD.GetComponent<Image>().color = new Color(1f, 1f, 1f, .3f);
+
+        pc = pc100;
+        mg = mg30;
+        fb = fb30;
     }
 
     // Update is called once per frame
@@ -23,20 +47,43 @@ public class PlayerShoot : MonoBehaviour
         GunSelection();
         Pistol();
         MachineGun();
+        Fireball();
     }
 
     private void Pistol()
     {
-        if (gunSelect == 1)
+        if (gunSelect != 1) { return; }
+        fb = fb100;
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                pSys.Play();
-                pSys.Emit(1);
-            }
+            pSys.Play();
+            pSys.Emit(1);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            //nothing yet.
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            pSys.emissionRate = 1f;
+            pSys.Stop();
+        }
+
+        
+    }
+
+    private void MachineGun()
+    {
+        if (gunSelect == 2)
+        {
             if (Input.GetMouseButton(0))
             {
-                //nothing yet.
+                pSys.emissionRate = 10f;
+                pSys.Play();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                //nothing yet
             }
             if (Input.GetMouseButtonUp(0))
             {
@@ -47,26 +94,25 @@ public class PlayerShoot : MonoBehaviour
         }
     }
 
-    private void MachineGun()
+    private void Fireball()
     {
-        if (gunSelect == 2)
+        print(gunSelect);
+        if(gunSelect != 3) { return; }
+        print("gun3");
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                pSys.Play();
-                pSys.Emit(1);
-            }
-            if (Input.GetMouseButton(0))
-            {
-                pSys.Play();
-                pSys.emissionRate = 10f;
-            }
-            if (Input.GetMouseButtonUp(0))
-            {
-                pSys.emissionRate = 1f;
-                pSys.Stop();
-            }
-
+            pSys.emissionRate = 1f;
+            pSys.Play();
+            pSys.Emit(1);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            // nothing yet
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            pSys.emissionRate = 1f;
+            pSys.Stop();
         }
     }
 
@@ -76,9 +122,9 @@ public class PlayerShoot : MonoBehaviour
         {
             gunSelect++;
             print($"gunSelect {gunSelect}");
-            if (gunSelect > 2)
+            if (gunSelect > 3)
             {
-                gunSelect = 0;
+                gunSelect = 1;
             }
         }
     }
